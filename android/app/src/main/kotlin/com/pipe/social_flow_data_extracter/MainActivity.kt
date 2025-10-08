1ab07dbc982bb446a953eb15cc90dcae3db600e6
Nirmal Mina <108892310+nirmal-mina@users.noreply.github.com>
2025-10-08 12:45:08 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/pipe/social_flow_data_extracter/MainActivity.kt
+++ android/app/src/main/kotlin/com/pipe/social_flow_data_extracter/MainActivity.kt
@@ -1,5 +1,27 @@
-package com.pipe.social_flow_data_extracter
+package com.artxdev.newpipeextractor_dart
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodCall
 
-class MainActivity : FlutterActivity()
+class NewpipeextractorDartPlugin: FlutterPlugin, MethodChannel.MethodCallHandler {
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(binding.binaryMessenger, "newpipeextractor_dart")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+    // implement plugin methods here, example:
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
